b797e681a89e945ed8cf174b9367126fdf9e2fd5
Fazlul Shahriar <fshahriar@gmail.com>
2017-10-29 17:36:56 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+[![GoDoc](https://godoc.org/github.com/fhs/Ghost?status.svg)](https://godoc.org/github.com/fhs/Ghost)
+
+## Overview
+
+This program is a
+[GhostText](https://github.com/GhostText/GhostTex) plugin for [Acme
+text editor](https://en.wikipedia.org/wiki/Acme_(text_editor)). See
+[Godoc](https://godoc.org/github.com/fhs/Ghost) for documentation.
---
+++ main.go
@@ -0,0 +1,116 @@
+/*
+This program is a GhostText plugin for Acme text editor.
+
+GhostText (https://github.com/GhostText/GhostTex) is a Chrome extension
+that allows you to edit text in web pages (e.g. in <textarea>) in various
+text editors. It interacts with the text editor via a websocket server,
+such as this program.
+
+By default, this program will listen on port 4001 (GhostText default
+port). This can be changed with the -port flag. When this program receives
+a new edit request from GhostText, it'll open a new acme window. Middle
+clicking on "Put" sends text from acme to the web page. After editing
+is done, when GhostText closes the websocket connection, the acme window
+will be automatically closed if it's not dirty.
+*/
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"9fans.net/go/acme"
+	"github.com/gorilla/websocket"
+)
+
+var port = flag.Int("port", 4001, "websocket listen port")
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return strings.HasPrefix(r.Header.Get("Origin"), "chrome-extension://")
+	},
+}
+
+type Selection struct {
+	Start, End int
+}
+
+type WebText struct {
+	Text       string
+	Selections []Selection
+	Title      string
+	URL        string
+}
+
+type AcmeText struct {
+	Text string `json:"text"`
+}
+
+func handleWinEvents(w *acme.Win, conn *websocket.Conn) {
+	for e := range w.EventChan() {
+		switch e.C2 {
+		case 'x', 'X': // execute
+			if string(e.Text) == "Put" {
+				body, err := w.ReadAll("body")
+				if err != nil {
+					log.Printf("failed to read body: %v\n", err)
+				}
+				if err := conn.WriteJSON(&AcmeText{Text: string(body)}); err != nil {
+					log.Printf("WriteJSON failed: %v\n", err)
+				}
+				w.Ctl("clean")
+				continue
+			}
+		}
+		w.WriteEvent(e)
+	}
+	w.CloseFiles()
+}
+
+func handler(wr http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Upgrade") != "websocket" {
+		wr.Header().Set("Content-Type", "application/json")
+		greeting := fmt.Sprintf(`{"WebSocketPort":%v,"ProtocolVersion":1}`, *port)
+		fmt.Fprintf(wr, "%v\n", greeting)
+		return
+	}
+	conn, err := upgrader.Upgrade(wr, r, nil)
+	if err != nil {
+		log.Printf("websocket upgrade failed: %v\n", err)
+		return
+	}
+	w, err := acme.New()
+	if err != nil {
+		log.Printf("acme.New failed: %v\n", err)
+		return
+	}
+	w.Write("tag", []byte("Undo Redo Put"))
+	go handleWinEvents(w, conn)
+	for {
+		var web WebText
+		if err := conn.ReadJSON(&web); err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseNoStatusReceived) {
+				log.Printf("error: %v, user-agent: %v", err, r.Header.Get("User-Agent"))
+			}
+			w.Del(false)
+			return
+		}
+		w.Name(fmt.Sprintf("/Ghost/%v", web.URL))
+		w.Addr(",")
+		w.Write("data", []byte(web.Text))
+		w.Ctl("clean")
+	}
+}
+
+func main() {
+	flag.Parse()
+	http.HandleFunc("/", handler)
+	if err := http.ListenAndServe(fmt.Sprintf("localhost:%v", *port), nil); err != nil {
+		log.Fatalf("ListenAndServe failed: %v", err)
+	}
+}
